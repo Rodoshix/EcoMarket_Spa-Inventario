56825f0a44f612954bb844c2f4aa5e3875c390ba
Rodoshix <102111944+Rodoshix@users.noreply.github.com>
2025-05-14 23:49:09 -0400

    Update ProductoRepositoryJPA.java

--- src/main/java/com/ecomarket/inventario/repository/ProductoRepositoryJPA.java
+++ src/main/java/com/ecomarket/inventario/repository/ProductoRepositoryJPA.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository // Marca esta interfaz como componente de acceso a datos
 public interface ProductoRepositoryJPA extends JpaRepository<Producto, Long> {
@@ -14,6 +15,9 @@ public interface ProductoRepositoryJPA extends JpaRepository<Producto, Long> {
 
     // Buscar una lista de productos por su categoría
     List<Producto> findByCategoria(String categoria);
+
+    // Buscar producto por nombre y descripción
+    Optional<Producto> findByNombreAndDescripcion(String nombre, String descripcion);
 
     // Spring Data JPA ya incluye automáticamente:
     // - findAll()
